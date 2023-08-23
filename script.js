ccf9eca0a0e311fa113e3ded3d72b1bff4836d1d
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-08-23 14:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const input = document.getElementsByTagName("input")[0];
+input.addEventListener("click", () => {
+    const colorSelect = document.getElementById("colorSelect");
+    colorSelect.remove(colorSelect);
+});
